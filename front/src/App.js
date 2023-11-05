18351f3c42712fa596cbc53898e5c5bc3f22b57c
Emmanuel Rokah <36491225+MiniManch@users.noreply.github.com>
2023-11-06 01:01:37 +0200

    Add files via upload

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './Style/App.css';
+import SpotifyApp from './Components/SpotifyApp';
+
+function App() {
+  return (
+    <div className="App">
+      <SpotifyApp/>
+    </div>
+  );
+}
+
+export default App;
