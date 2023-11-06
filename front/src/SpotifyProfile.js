afbafa3571422a543c3b73b1a74ffb72e7fb2822
Emmanuel Rokah <36491225+MiniManch@users.noreply.github.com>
2023-11-06 21:15:25 +0200

    Authorization

---
+++ front/src/SpotifyProfile.js
@@ -0,0 +1,48 @@
+// SpotifyProfile.js
+import React, { useState, useEffect } from 'react';
+import css from '../Style/SpotifyProfile.module.css';
+
+const SpotifyProfile = () => {
+    const [userData, setUserData] = useState(null);
+
+    const fetchUserProfile = async () => {
+        const token = localStorage.getItem('access_token');
+        if (token){
+            try {
+                const response = await fetch('https://api.spotify.com/v1/me', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                if (response.ok) {
+                    const userData = await response.json();
+                    setUserData(userData);
+                } else {
+                    console.error('Failed to fetch user profile');
+                }
+            } catch (error) {
+                console.error('Error fetching user profile:', error);
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchUserProfile();
+    }, []);
+
+    return (
+        <>
+            { 
+                userData ?
+                    <div className={css.profile_container}>
+                        <img className={css.profile_image} src={userData.images[0].url} alt="Profile" />
+                        <p className={css.profile_name}>{userData.display_name}</p>
+                    </div>
+                    :
+                null
+            }
+        </>
+    );
+};
+
+export default SpotifyProfile;
